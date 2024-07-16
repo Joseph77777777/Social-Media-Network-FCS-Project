f8079b7b9fd910fc808404a027a81911e7f2514a
Joseph <josephnakhle1990@gmail.com>
2024-07-16 14:49:19 +0300

    Implementing add_friend function to add friends

--- user.py
+++ user.py
@@ -9,3 +9,27 @@ class User:
         self.fullName = fullName
         self.friendsList = []
         self.posts = posts
+        self.size_friendsList=0
+
+    def add_friend(self,friend_Id,friend_Name):
+        # adding friend to the friend list
+        # parameter friend_Id: ID for the new friend
+        # parameter friend_Name: Name of the new friend
+
+        friend_exist=False
+        for  f  in self.friendsList:#Traversing the list to check if the friend index exist in the list
+            if f[0]==friend_Id :
+                friend_exist=True
+                break
+        
+        if  friend_exist==False:#if not exist it will be added to the list
+            self.friendsList.append(friend_Id,friend_Name)
+        else:
+            print("The friend with ID"+str(friend_Id)+"and name "+friend_Name+"is already added")
+            
+       
+
+
+
+    
+        
