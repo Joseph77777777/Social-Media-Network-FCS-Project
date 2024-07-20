898f294b2a16edd08a8410696dcf39c80ba4b6ee
Joseph <josephnakhle1990@gmail.com>
2024-07-20 09:42:02 +0300

    Adding a new function to add interests of the user

--- Structure/user.py
+++ Structure/user.py
@@ -1,14 +1,16 @@
 class User:
-    def __init__(self, userId, fullName, posts=None):
+    def __init__(self, userId, fullName, posts=None,interests=None):
         # Initializing a new user.
         # parameter userId: ID for the user
         # parameter fullName: Name of the user
         # parameter posts:  posts made by the user 
+        # parameter interests : Interests of the user
         
         self.userId = userId
         self.fullName = fullName
         self.friendsList = []
         self.posts = []
+        self.interests=[]
     
 
     def add_friend(self,friend_Id,friend_Name):
@@ -46,12 +48,16 @@ class User:
     def post_Update(self,new_post):
         # adding new post to the users post
         self.posts.append(new_post)
+    
+    def interests_Update(self,new_interest):
+        #adding new interest to the users interests
+        self.interests.append(new_interest)
 
     def __str__(self) :
         friendsList=" "
         for friend_Id,friend_Name in self.friendsList:
             friendsList+="ID: "+str(friend_Id)+"/"+friend_Name+","
-        return "User(" + str(self.userId) + ", " + self.fullName +","+str(self.posts) + ", Friends: [" + friendsList + "])"
+        return "User(" + str(self.userId) + ", " + self.fullName +","+str(self.posts) + ","+str(self.interests) +", Friends: [" + friendsList + "])"
     
 
 
