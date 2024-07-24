af95fc9d63ede12a337ff8d9271a786702b89260
Joseph <josephnakhle1990@gmail.com>
2024-07-24 20:22:43 +0300

    Adding docstring to the class User

--- Structure/user.py
+++ Structure/user.py
@@ -1,42 +1,50 @@
 class User:
-    def __init__(self, userId, fullName, posts,interests):
-        # Initializing a new user.
-        # parameter userId: ID for the user
-        # parameter fullName: Name of the user
-        # parameter posts:  posts made by the user 
-        # parameter interests : Interests of the user
-        
+    def __init__(self, userId, fullName, posts=None,interests=None):
+        """  
+        Initializing a new user.
+        parameters:
+                userId: ID for the user
+                fullName: Name of the user
+                posts: posts made by the user 
+                interests : Interests of the user
+         """
         self.userId = userId
         self.fullName = fullName
         self.friendsList = []
         self.posts = []
-        self.interests=interests
+        self.interests=[]
         
     
 
     def add_friend(self,friend_Id,friend_Name,weight):
-        # adding friend to the friend list
-        # parameter friend_Id: ID for the new friend
-        # parameter friend_Name: Name of the new friend
-
+        """
+         Adding friend to the friend list
+         parameters:
+                    friend_Id: ID for the new friend
+                    friend_Name: Name of the new friend
+                    weight : Weight of the relationship.
+        """
         friend_exist=False
-        for  f  in self.friendsList:#Traversing the list to check if the friend  exist in the list
+        #Traversing the list to check if the friend  exist in the list
+        for  f  in self.friendsList:
             if f[0]==friend_Id :
                 friend_exist=True
                 break
-        
-        if  friend_exist==False:#if not exist it will be added to the list
+        #If not exist it will be added to the list
+        if  friend_exist==False:
             self.friendsList.append((friend_Id,friend_Name,weight))
             
         else:
             print("The friend with ID: "+str(friend_Id)+" is already added")
 
     def remove_friend(self,friend_Id):
-        # removing friend from the friend list
-        # parameter friend_Id: ID for the existing friend
-        # parameter friend_Name: Name of the existing friend
-
+        """
+        removing a friend from the friends list
+            parameter:
+                friend_Id: friends Id to be removed
+        """
         friend_exist = False
+        #Checking if the friend exist in the list of friend
         for f in self.friendsList:
             if f[0] == friend_Id:
                 friend_exist = True
@@ -47,14 +55,23 @@ class User:
             print("The friend with ID: " + str(friend_Id) + " doesn't exist")
             
     def post_Update(self,new_post):
-        # adding new post to the users post
+        """adding new post to the users post
+              parameter:
+                    new_post:content of the new post
+        """
         self.posts.append(new_post)
     
     def interests_Update(self,new_interest):
-        #adding new interest to the users interests
+        """adding new interest to the users interests
+                parameter:
+                   new_interest: content of the new interest         
+        """
         self.interests.append(new_interest)
 
     def __str__(self) :
+        """
+        Return a string representing  the user, including their ID, name, posts, interests, and friends.
+        """
         friendsList=" "
         for friend_Id,friend_Name in self.friendsList:
             friendsList+="ID: "+str(friend_Id)+"/"+friend_Name+","
